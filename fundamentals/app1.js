e242f5b57e5da387f47ca3a132b710a265178fa0
ahmed-razic <ahmed.razic@gmail.com>
2022-03-25 11:41:29 +0100

    initial commit

---
+++ fundamentals/app1.js
@@ -0,0 +1,16 @@
+console.log('Hi');
+console.log(125.4);
+console.log(true);
+console.log(null);
+console.log(undefined);
+
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: 1, b: 2, c: 3, d: 4, e: 5 });
+
+console.error('an error');
+console.warn('a warning');
+console.table({ a: 1, b: 2, c: 3, d: 4, e: 5 });
+
+console.time('a');
+console.log(2 ** 2000000000);
+console.timeEnd('a');
---
+++ fundamentals/app2.js
@@ -0,0 +1,30 @@
+var name1 = 'Ahmed';
+console.log(name1);
+
+name1 = 'Razic';
+console.log(name1);
+
+let greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+const PI = 3.1415;
+console.log(PI);
+
+//PI = 3;
+
+const user = { a: 2, b: 4, c: 6 };
+console.table(user);
+
+user.c = 8;
+console.table(user);
+
+const numbers = [1, 3, 5, 7, 9];
+console.log(numbers);
+
+numbers.push(11);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
