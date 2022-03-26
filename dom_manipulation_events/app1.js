f6980432cdc4819ac9590900828884ae90ee4827
ahmed-razic <ahmed.razic@gmail.com>
2022-03-26 15:08:17 +0100

    dom selectors

---
+++ dom_manipulation_events/app1.js
@@ -0,0 +1,18 @@
+let val = document;
+
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.children;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+val = document.links;
+val = document.links[2].setAttribute('href', 'http://google.com');
+
+val = document.links[4].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+console.log(val);
