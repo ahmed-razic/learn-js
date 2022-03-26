69e917e1eef4c46b835760479f323f2261a5f155
ahmed-razic <ahmed.razic@gmail.com>
2022-03-26 10:00:56 +0100

    window object

---
+++ fundamentals/app10.js
@@ -0,0 +1,21 @@
+console.log(window);
+// window.alert('Heyy');
+// window.prompt('Age?');
+console.log(window.document);
+
+console.log(window.history);
+
+//window.location.href = 'http://google.com';
+
+let val = window;
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.history.length;
+val = window.navigator;
+val = window.navigator.vendor;
+val = window.navigator.platform;
+val = window.navigator.language;
+
+console.log(val);
