7bc2008f4a5739b324d9ad7dbd967eddaf1f6890
ahmed-razic <ahmed.razic@gmail.com>
2022-03-28 22:04:51 +0200

    numbers

---
+++ numbers/script.js
@@ -0,0 +1,30 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+val = Math.PI;
+val = Math.E;
+val = Math.SQRT2;
+val = Math.LOG10E;
+val = Math.SQRT1_2;
+
+val = Math.sin(0.5);
+val = Math.cos(0.5);
+val = Math.tan(0.5);
+val = Math.round(2.4);
+val = Math.ceil(2.8);
+val = Math.floor(2.8);
+
+val = Math.floor(Math.random() * 20 + 1);
+
+val = Math.max(23, 2, -5.4);
+val = Math.min(23, 2, -5.4);
+
+val = Math.floor(Math.random() * 100 + 1);
+console.log(val);
